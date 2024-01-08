129023aa39e1224c0faa6efc21e422f5c8fbfaf4
Judah Kahler <judah.kahler@gmail.com>
2024-01-08 09:49:11 -0600

    First loop finished

--- language_essentials/fundamentals_one/Program.cs
+++ language_essentials/fundamentals_one/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+// First Loop
 Console.WriteLine("Loop that prints the values between 1-255");
 
-
+for(int i = 1; i <= 255; i++)
+{
+    Console.WriteLine(i);
+}
--- language_essentials/fundamentals_one/obj/Debug/net8.0/fundamentals_one.AssemblyInfo.cs
+++ language_essentials/fundamentals_one/obj/Debug/net8.0/fundamentals_one.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fundamentals_one")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be57c4ba5dde3a533be4a9933f90aeb7554ab48")]
 [assembly: System.Reflection.AssemblyProductAttribute("fundamentals_one")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fundamentals_one")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
