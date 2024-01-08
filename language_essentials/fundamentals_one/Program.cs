8903cc6a7b1ac6cec5512f4c5140dd1c257e08ef
Judah Kahler <judah.kahler@gmail.com>
2024-01-08 10:57:01 -0600

    Algos incomplete

--- language_essentials/fundamentals_one/Program.cs
+++ language_essentials/fundamentals_one/Program.cs
@@ -7,3 +7,11 @@ for(int i = 1; i <= 255; i++)
 {
     Console.WriteLine(i);
 }
+
+// Second Loop 
+Console.WriteLine("Loop that prints 5 random values between 10-20")
+
+for(int i = 1; i <= 5; i++)
+{
+    Console.WriteLine("")
+}
