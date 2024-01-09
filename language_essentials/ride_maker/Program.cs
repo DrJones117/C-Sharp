607771e311bbfb9c1cc68ddefe3517958420f9c1
Judah Kahler <judah.kahler@gmail.com>
2024-01-09 13:35:21 -0600

    Finished Ride Maker assignment

---
+++ language_essentials/ride_maker/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+Vehicle Truck = new("Ford V8", 3, "Gray", true);
+Truck.PrintInfo();
+
+Vehicle CompactCar = new("Honda Civic", 7, "Blue", true);
+CompactCar.PrintInfo();
+
+Vehicle StockCar1 = new("TSLA", "Red");
+StockCar1.PrintInfo();
+
+Vehicle StockCar2 = new("TM", "BLack");
+StockCar2.PrintInfo();
+
+List<Vehicle> vehicleList = [Truck, CompactCar, StockCar1, StockCar2];
+
+foreach (Vehicle entry in vehicleList)
+{
+    entry.PrintInfo();
+}
+
+Truck.Travel(100);
+Truck.PrintInfo();
