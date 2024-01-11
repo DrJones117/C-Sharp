d30d94f5c3dbde757015a9eb9d6663804dcb3ee2
Judah Kahler <judah.kahler@gmail.com>
2024-01-11 12:15:32 -0600

    started ViewModel Fun assignment

---
+++ asp_mvc/view_model_fun/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace view_model_fun.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
