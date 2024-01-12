f99901ad6ac5ac8f2a4da7767181d3ed6c1ec7b8
Judah Kahler <judah.kahler@gmail.com>
2024-01-12 12:27:12 -0600

    Started Date Validator assignment

---
+++ asp_mvc/date_validator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace date_validator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
